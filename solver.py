b7919741aa5e57a661e0d125c7aa7ab2f194f4df
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-09-27 11:29:37 -0400

    Add function to check if puzzle is in goal state

--- solver.py
+++ solver.py
@@ -1,11 +1,10 @@
-
-
 # Take puzzle in as input from user
 # Split input by spaces and convert all elements to int
 puzzle = [int(x) for x in input().split()]
 
 print (puzzle)
 
+# Checks if puzzle is valid ie contains 0 up to 11
 def valid(p):
     if (len(p) != 12):
         return False
@@ -15,4 +14,13 @@ def valid(p):
                 return False
         return True
 
+# Checks if puzzle is in goal state
+def goal(p):
+    for x in range(1, 11):
+        if p[x-1] != x:
+            return False
+    return True
+
 print ("Is puzzle valid? "+str(valid(puzzle)))
+print ("Is puzzle in goal state? "+str(goal(puzzle)))
+
