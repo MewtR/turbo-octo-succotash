c48fa04dcc954a8491769d49435391cf043ee64d
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-10-01 11:58:50 -0400

    Initial implementation of A*. Might still need some work.

--- solver.py
+++ solver.py
@@ -4,28 +4,36 @@ import depth_first
 import time
 import heuristics
 import best_first
+import a_star
 # Take puzzle in as input from user
 # Split input by spaces and convert all elements to int
 puzzle = [int(x) for x in input().split()]
 
-root_node = Node(puzzle, None, '0')
+root_node = Node(puzzle, None, '0', 0)
 
 #Example puzzles:
 #DFS
 # 0 2 3 4 1 6 7 8 5 9 10 11 <- solved instantaneously
 # 0 2 3 4 1 5 7 8 6 9 10 11 <- takes about 30 secs to solve
+# 9 0 6 1 3 10 8 11 2 5 7 4 <- ??? (way too long)
 
 #BFS with sum of permutation of inversions
 # 0 2 3 4 1 6 7 8 5 9 10 11 <- solved instantaneously
 # 0 2 3 4 1 5 7 8 6 9 10 11 <- solved instantaneously
+# 9 0 6 1 3 10 8 11 2 5 7 4 <- solved instantaneously
 
 print ("Is puzzle valid? "+str(utilities.valid(root_node)))
 print ("Is puzzle in goal state? "+str(utilities.goal(root_node)))
+#A*
+start_time = time.time()
+goal_node = a_star.search(root_node)
+solution_path = utilities.find_solution_path(goal_node)
+utilities.write_to_file(solution_path, 'puzzleA_star.txt')
+print ("Time to finish A*: {}".format(time.time()-start_time))
+#BFS
 start_time = time.time()
 goal_node = best_first.search(root_node)
-
 solution_path = utilities.find_solution_path(goal_node)
 utilities.write_to_file(solution_path, 'puzzleBFS.txt')
-
-print ("Program execution time: {}".format(time.time()-start_time))
-
+print ("Time to finish BFS: {}".format(time.time()-start_time))
+#goal_node = depth_first.search(root_node)
--- utilities.py
+++ utilities.py
@@ -35,53 +35,53 @@ def generateChildren(p):
         up_left_index = zero_index-5
         up_left_child = p.config.copy()
         up_left_child = swap(up_left_child, up_left_index, zero_index)
-        children.append(Node(up_left_child, p, chr(up_left_index+97)))
+        children.append(Node(up_left_child, p, chr(up_left_index+97), p.cost_so_far+1))
     #Up index-4 is tile above tile at index
     if (zero_index - 4 > -1):
         up_index = zero_index-4
         up_child = p.config.copy()
         up_child = swap(up_child, up_index, zero_index)
-        children.append(Node(up_child, p, chr(up_index+97)))
+        children.append(Node(up_child, p, chr(up_index+97), p.cost_so_far+1))
     #Up right is index - 3
     # if blank at index 3, 7 or 11, can't go up right
     if ((zero_index - 3 > 0) and ((zero_index-3) % 4 != 0)):
         up_right_index = zero_index-3
         up_right_child = p.config.copy()
         up_right_child = swap(up_right_child, up_right_index, zero_index)
-        children.append(Node(up_right_child, p, chr(up_right_index+97)))
+        children.append(Node(up_right_child, p, chr(up_right_index+97), p.cost_so_far+1))
     #Left is index-1
     #indices 0, 4 and 8 can't go left
     if ((zero_index % 4 != 0)):
         left_index = zero_index-1
         left_child = p.config.copy()
         left_child = swap(left_child, left_index, zero_index)
-        children.append(Node(left_child, p, chr(left_index+97)))
+        children.append(Node(left_child, p, chr(left_index+97), p.cost_so_far+1))
     #Right is index+1
     #indices 3, 7 and 11 can't go right
     if ((zero_index % 4 != 3)):
         right_index = zero_index+1
         right_child = p.config.copy()
         right_child = swap(right_child, right_index, zero_index)
-        children.append(Node(right_child, p, chr(right_index+97)))
+        children.append(Node(right_child, p, chr(right_index+97), p.cost_so_far+1))
     #Down left is index+3
     if ((zero_index + 3 < 11) and (zero_index % 4 != 0 )):
         down_left_index = zero_index+3
         down_left_child = p.config.copy()
         down_left_child = swap(down_left_child, down_left_index, zero_index)
-        children.append(Node(down_left_child, p, chr(down_left_index+97)))
+        children.append(Node(down_left_child, p, chr(down_left_index+97), p.cost_so_far+1))
     #Down right is index+5
     #indices 3, 7 and 11 can't go down right
     if ((zero_index + 5 < 12) and (zero_index != 3)):
         down_right_index = zero_index+5
         down_right_child = p.config.copy()
         down_right_child = swap(down_right_child, down_right_index, zero_index)
-        children.append(Node(down_right_child, p, chr(down_right_index+97)))
+        children.append(Node(down_right_child, p, chr(down_right_index+97), p.cost_so_far+1))
     #Down is index+4
     if ((zero_index + 4 < 12)):
         down_index = zero_index+4
         down_child = p.config.copy()
         down_child = swap(down_child, down_index, zero_index)
-        children.append(Node(down_child, p, chr(down_index+97)))
+        children.append(Node(down_child, p, chr(down_index+97), p.cost_so_far+1))
 
     return children
 
@@ -115,4 +115,4 @@ def find_solution_path(goal_node):
 def write_to_file(solution_path, file_name):
     with open(file_name, 'w') as f:
         for x in solution_path:
-            f.write("{} {} \n".format(x.move,x.config))
+            f.write("{} {} cost: {}\n".format(x.move,x.config, x.cost_so_far))
